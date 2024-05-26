5b2ac20204caba93b599710947565f9a6fe253c5
David Konsumer <konsumer@jetboystudio.com>
2024-05-25 23:01:35 -0700

    examples

--- examples/siouxsie_arduino/siouxsie_arduino.ino
+++ examples/siouxsie_arduino/siouxsie_arduino.ino
@@ -1,6 +1,6 @@
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
 #include "image_siouxsie.h"
 
@@ -13,18 +13,22 @@
 // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 #define SCREEN_ADDRESS 0x3D
 
-// Reset pin # (or -1 if sharing Arduino reset pin)
-#define OLED_RESET -1
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int frame = 0;
 
-void setup() {}
+void setup() {
+  Serial.begin(9600);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+}
 
 void loop() {
   display.clearDisplay();
-  display.drawBitmap(0, 0, image_siouxsie[frame++ % image_siouxsie_size], SCREEN_WIDTH, SCREEN_HEIGHT, 1);
+  display.drawBitmap(0, 0, image_siouxsie[frame++ % image_siouxsie_size], SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
   display.display();
   delay(100);
 }
